60c25b9d6afaa7167e52561fa6d7af08444b59d1
dadar870218 <dadar870218@gmail.com>
2024-10-09 21:05:29 +0200

    added logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link';
+import { FaHotel } from "react-icons/fa";
+import { Button } from '../ui/button';
 
 function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <FaHotel className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 }
-
 export default Logo

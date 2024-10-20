15623f76b4ce3abcd942b0469f49627e65616293
dadar870218 <dadar870218@gmail.com>
2024-10-20 23:33:48 +0200

    supabase , profileimage update , imageSchema

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,10 @@
 import FormContainer from '@/components/form/FormContainer';
-import { updateProfileAction, fetchProfile } from '@/utils/actions';
+import {
+  updateProfileAction,
+  fetchProfile,
+  updateProfileImageAction,
+} from '@/utils/actions';
+import ImageInputContainer from '@/components/form/ImageinputContainer';
 import FormInput from '@/components/form/FormInput';
 import { SubmitButton } from '@/components/form/Buttons';
 
@@ -10,8 +15,12 @@ async function ProfilePage() {
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>user profile</h1>
       <div className='border p-8 rounded-md'>
-        {/* image input container */}
-
+        <ImageInputContainer
+  image={profile.profileImage}
+  name={profile.username}
+  action={updateProfileImageAction}
+  text='Update Profile Image'
+/>;
         <FormContainer action={updateProfileAction}>
           <div className='grid gap-4 md:grid-cols-2 mt-4 '>
             <FormInput

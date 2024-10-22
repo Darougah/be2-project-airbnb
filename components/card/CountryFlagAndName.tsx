cd01904da7717da17d62a370204f504b4a47b95c
dadar870218 <dadar870218@gmail.com>
2024-10-22 11:58:55 +0200

    propertycard and extra favorite and flag

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+// import { findCountryByCode } from '@/utils/countries';
 
-function CountryFlagAndName() {
+// function CountryFlagAndName({ countryCode }: { countryCode: string }) {
+//   const validCountry = findCountryByCode(countryCode);
+//   const countryName =
+//     validCountry!.name.length > 20
+//       ? `${validCountry!.name.substring(0, 20)}...`
+//       : validCountry!.name;
+//   return (
+//     <span className='flex justify-between items-center gap-2 text-sm '>
+//       {validCountry?.flag} {countryName}
+//     </span>
+//   );
+// }
+// export default CountryFlagAndName;
+
+import { findCountryByCode } from '@/utils/countries';
+import Flag from 'react-world-flags';
+
+function CountryFlagAndName({ countryCode }: { countryCode: string }) {
+  const validCountry = findCountryByCode(countryCode);
+
+  const countryName =
+    validCountry && validCountry.name.length > 20
+      ? `${validCountry.name.substring(0, 20)}...`
+      : validCountry?.name || 'Unknown Country';
+
   return (
-    <div>CountryFlagAndName</div>
-  )
+    <span className='flex items-center gap-2 text-sm'>
+      {validCountry && (
+        <Flag code={validCountry.code} style={{ width: 20, height: 20 }} />
+      )}
+      {countryName}
+    </span>
+  );
 }
 
-export default CountryFlagAndName
+export default CountryFlagAndName;
